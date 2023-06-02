c3177ae058a9aea2dd40d21e52b2ccdb69d9e28e
Gagandeep kaur <gagan.bhandal@ymail.com>
2023-06-01 22:24:25 -0500

    Dictionary

---
+++ module.py
@@ -0,0 +1,15 @@
+# Create a module in VS code and import it into jupyter notebook
+# 1. Has a function to calculate the square footage of a house? 
+
+def square_footage():
+l == 2500
+w == 3000
+square_footage == l * w
+print(square_footage)
+
+#Has a function to calculate the circumference of a circle 2 Pi r?
+def c_of_circle():
+r = 10
+c_of_circle = 2*pi*r
+print (c_of_circle)
+
